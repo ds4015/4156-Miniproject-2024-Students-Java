adcb16b5c0d6cf417c4953643a2eb80b77597bd1
ds4015 <166780943+ds4015@users.noreply.github.com>
2024-09-09 02:00:35 -0400

    Cleaned up code to adhere to style guidelines (#1)
    Corrected import statements, line lengths, removal of tabs, added missing Javadoc comments

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -1,10 +1,13 @@
 package dev.coms4156.project.individualproject;
 
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-
-import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Test class for Course.java
+ */
 
 @SpringBootTest
 @ContextConfiguration
